9ab37c41abfc507e1bd1e7ef64817fa97574663a
Tess Elation <tessiselated@users.noreply.github.com>
2022-03-27 13:24:50 +1100

    Success and failure messages

--- src/GameCard.tsx
+++ src/GameCard.tsx
@@ -2,6 +2,7 @@ import React, { Dispatch, SetStateAction, useState } from "react";
 import { Card } from "primereact/card";
 import { InputText } from "primereact/inputtext";
 import { Button } from "primereact/button";
+import zip from "lodash/zip";
 import { VerbSelection } from "./utils/chooseVerbs";
 
 export function GameCard({
@@ -22,15 +23,48 @@ export function GameCard({
       <p>
         Enter {form} form for {english}:
       </p>
-      <InputText value={answer} onChange={(e) => setAnswer(e.target.value)} />
+      <InputText
+        value={answer}
+        style={{ textAlign: "center" }}
+        onChange={(e) => setAnswer(e.target.value)}
+      />
       <br />
       <Button
         label="Check!"
         className="p-button-outlined"
         onClick={() => setCorrect(answer === verbDetails.forms[form].join(""))}
       />
-      {correct === true && <p>Yay!</p>}
-      {correct === false && <p>Boo hiss</p>}
+      {correct === true && (
+        <p className="success-text">
+          <ruby>
+            正<rt>ただ</rt>しい！！
+          </ruby>
+          - {furiganadVerb(verbDetails.forms[form], verbDetails.furigana)}
+        </p>
+      )}
+      {correct === false && (
+        <p className="failure-text">
+          <ruby>
+            違<rt>ちが</rt>う
+          </ruby>
+          - {furiganadVerb(verbDetails.forms[form], verbDetails.furigana)}
+        </p>
+      )}
     </Card>
+  );
+}
+
+function furiganadVerb(verb: string[], furigana: string[] = []) {
+  const verbAndFurigana = zip(verb, furigana);
+  return (
+    <ruby>
+      {verbAndFurigana.map(([verbPortion, pronunciation]) => {
+        return (
+          <>
+            {verbPortion} {pronunciation && <rt>{pronunciation}</rt>}
+          </>
+        );
+      })}
+    </ruby>
   );
 }
